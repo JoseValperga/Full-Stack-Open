83e9783bc89203e199162c8cba5694e0fd5b11f3
JoseValperga <jose.valperga@gmail.com>
2024-01-04 14:49:05 -0300

    Hasta ejercicio 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,8 +5,73 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [totalClicks, setTotalClicls] = useState(0);
 
-  return <div>code here</div>;
+  const handleGoodClick = () => {
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    setTotalClicls(updatedGood + neutral + bad);
+  };
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    setTotalClicls(good + updatedNeutral + bad);
+  };
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setTotalClicls(good + neutral + updatedBad);
+  };
+
+  const Statisctics = ({ good, neutral, bad, totalClicks }) => {
+    if (totalClicks === 0) {
+      return "No feedback given";
+    }
+    
+    return (
+      <ul>
+        <li>
+          {"good"} {good}
+        </li>
+        <li>
+          {"neutral"} {neutral}
+        </li>
+        <li>
+          {"bad"} {bad}
+        </li>
+        <li>
+          {"all"} {totalClicks}
+        </li>
+        <li>
+          {"average "}
+          {totalClicks > 0 ? (good * 1 + bad * -1) / totalClicks : " "}
+        </li>
+        <li>
+          {"positive "}
+          {totalClicks > 0 ? ((good * 1) / totalClicks) * 100 : " "}
+          {"%"}
+        </li>
+      </ul>
+    );
+  };
+
+  return (
+    <div>
+      <h1>{"give feedback"}</h1>
+      <button onClick={handleGoodClick}> good </button>
+      <button onClick={handleNeutralClick}> neutral </button>
+      <button onClick={handleBadClick}> bad </button>
+      <h1>statistics</h1>
+      <Statisctics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        totalClicks={totalClicks}
+      />
+    </div>
+  );
 };
 
 export default App;
