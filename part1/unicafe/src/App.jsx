7ce8f614587272e7222d4eae69c81ab814421707
JoseValperga <jose.valperga@gmail.com>
2024-01-05 20:21:46 -0300

    Hasta ejercicio 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,47 @@
 import { useState } from "react";
 
+const StatisticLine = ({ text, value }) => {
+  const newValue = Number.isInteger(value)
+    ? value
+    : parseFloat(value.toFixed(1));
+
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{text !== "positive " ? newValue : newValue + "%"}</td>
+    </tr>
+  );
+};
+
+const Statisctics = ({ good, neutral, bad, totalClicks }) => {
+  const goodValue = 1;
+  const badValue = -1;
+
+  const average = (good * goodValue + bad * badValue) / totalClicks;
+  const positive = (good * goodValue * 100) / totalClicks;
+
+  if (totalClicks === 0) {
+    return "No feedback given";
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text={"good "} value={good} />
+        <StatisticLine text={"neutral "} value={neutral} />
+        <StatisticLine text={"bad "} value={bad} />
+        <StatisticLine text={"all "} value={totalClicks} />
+        <StatisticLine text={"average "} value={average} />
+        <StatisticLine text={"positive "} value={positive} />
+      </tbody>
+    </table>
+  );
+};
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
@@ -25,50 +67,12 @@ const App = () => {
     setTotalClicls(good + neutral + updatedBad);
   };
 
-  const StatisticLine = ({ text, value }) => {
-    const newValue = Number.isInteger(value)
-      ? value
-      : parseFloat(value.toFixed(1));
-
-    return (
-      <tr>
-        <td>{text}</td>
-        <td>{text !== "positive " ? newValue : newValue + "%"}</td>
-      </tr>
-    );
-  };
-
-  const Statisctics = ({ good, neutral, bad, totalClicks }) => {
-    const goodValue = 1;
-    const badValue = -1;
-
-    const average = (good * goodValue + bad * badValue) / totalClicks;
-    const positive = (good * goodValue * 100) / totalClicks;
-
-    if (totalClicks === 0) {
-      return "No feedback given";
-    }
-
-    return (
-      <table>
-        <tbody>
-          <StatisticLine text={"good "} value={good} />
-          <StatisticLine text={"neutral "} value={neutral} />
-          <StatisticLine text={"bad "} value={bad} />
-          <StatisticLine text={"all "} value={totalClicks} />
-          <StatisticLine text={"average "} value={average} />
-          <StatisticLine text={"positive "} value={positive} />
-        </tbody>
-      </table>
-    );
-  };
-
   return (
     <div>
       <h1>{"give feedback"}</h1>
-      <button onClick={handleGoodClick}> good </button>
-      <button onClick={handleNeutralClick}> neutral </button>
-      <button onClick={handleBadClick}> bad </button>
+      <Button handleClick={handleGoodClick} text={" good "} />
+      <Button handleClick={handleNeutralClick} text={" neutral "} />
+      <Button handleClick={handleBadClick} text={" bad "} />
       <h1>statistics</h1>
       <Statisctics
         good={good}
