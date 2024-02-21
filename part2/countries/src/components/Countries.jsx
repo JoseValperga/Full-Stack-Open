9ee5feedf23780c2585d2df195abef72910bcf02
JoseValperga <jose.valperga@gmail.com>
2024-02-21 16:06:09 -0300

    part2 ready!

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -6,26 +6,18 @@ import axios from "axios";
 const Countries = ({ countries }) => {
   const [temp, setTemp] = useState(null);
   const [wind, setWind] = useState(null);
-  const [icon, setIcon] = useState(null);
+  const [icon, setIcon] = useState("");
 
   const apiKey = import.meta.env.VITE_API_KEY;
   const URL = import.meta.env.VITE_URL;
   const URL_ICON_1 = import.meta.env.VITE_URL_ICON_1;
   const URL_ICON_2 = import.meta.env.VITE_URL_ICON_2;
-
-  //const lat = countries[0].latlng[0];
-  //const lon = countries[0].latlng[1];
-
   const name = countries[0].capital[0];
-  console.log(name);
-
-  console.log(`${URL}q=${name}&units=metric&APPID=${apiKey}`);
 
   useEffect(() => {
     axios
       .get(`${URL}q=${name}&APPID=${apiKey}`)
       .then((response) => {
-        console.log("Clima", response.data);
         setTemp(response.data.main.temp - 273.14);
         setWind(response.data.wind.speed);
         setIcon(response.data.weather[0].icon);
@@ -35,44 +27,38 @@ const Countries = ({ countries }) => {
       });
   }, [apiKey, URL, name]);
 
-  console.log(`${URL_ICON_1}${icon}${URL_ICON_2}`);
-
-  /*useEffect(() => {
-    if (icon) {
-      axios.get(`${URL_ICON_1}${icon}${URL_ICON_2}`).then((response) => {
-        console.log(response);
-        console.log(response.data);
-        setIconSrc(response.data);
-      });
-    }
-  }, [URL_ICON_1, URL_ICON_2, icon]);
-*/
-  return (
-    <div>
-      {countries.map((countryData) => (
-        <div key={countryData.name.common}>
-          <h2>{countryData.name.common}</h2>
-          <p>Capital: {countryData.capital}</p>
-          <p>Population: {countryData.population}</p>
-          <p>Region: {countryData.region}</p>
-          <p>Area: {countryData.area}</p>
-          <h3>Languages</h3>
-          <Languages languages={countryData.languages} />
-          <img src={countryData.flags.png} alt="Country flag" />
-          <h3>Weather in {name}</h3>
-          <p>
-            Temp:{temp.toFixed(1)}
-            {" °C"}
-          </p>
-          <img src={`${URL_ICON_1}${icon}${URL_ICON_2}`} />
-          <p>
-            Wind:{wind}
-            {" m/s"}
-          </p>
-        </div>
-      ))}
-    </div>
-  );
+  console.log("icon", icon);
+  if (icon) {
+    return (
+      <div>
+        {countries.map((countryData) => (
+          <div key={countryData.name.common}>
+            <h2>{countryData.name.common}</h2>
+            <p>Capital: {countryData.capital}</p>
+            <p>Population: {countryData.population}</p>
+            <p>Region: {countryData.region}</p>
+            <p>Area: {countryData.area}</p>
+            <h3>Languages</h3>
+            <Languages languages={countryData.languages} />
+            <img src={countryData.flags.png} alt="Country flag" />
+            <h3>Weather in {name}</h3>
+            <p>
+              Temp:{temp.toFixed(1)}
+              {" °C"}
+            </p>
+            <img
+              src={icon ? `${URL_ICON_1}${icon}${URL_ICON_2}` : " "}
+              alt="Weather icon"
+            />
+            <p>
+              Wind:{wind}
+              {" m/s"}
+            </p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 Countries.propTypes = { countries: PropTypes.array.isRequired };
