8e9187d33040551e8c7d0f02cae29748b42a4f86
JoseValperga <jose.valperga@gmail.com>
2024-02-27 17:03:20 -0300

    exercises 3.13  3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,11 +5,11 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const Person = require("./models/person");
+const person = require("./models/person");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
-
 
 morgan.token("req-body", (req) => {
   if (req.method === "POST") {
@@ -24,7 +24,7 @@ app.use(
     ":method :url :status :res[content-length] - :response-time ms - :req-body"
   )
 );
-
+/*
 let persons = [
   {
     id: 1,
@@ -47,22 +47,27 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+*/
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((people) => {
     response.json(people);
   });
 });
-
+/*
 app.get("/info", (request, response) => {
+  
+  Person.find({}).then((persons)=>{
   const quantity = persons.length;
   const horaActual = new Date().toUTCString();
   const responseHTML = `
   <p>Phonebook has info for ${quantity} people<p>
   <p>${horaActual}<p>`;
-  response.send(responseHTML);
+  response.send(responseHTML)});
 });
+*/
 
+/*
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
@@ -72,6 +77,18 @@ app.get("/api/persons/:id", (request, response) => {
     response.status(404).end();
   }
 });
+*/
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  Person.findById(request.params.id).then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  });
+});
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
@@ -79,7 +96,7 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
 });
-
+/*
 app.post("/api/persons", (request, response) => {
   const person = request.body;
   const { name, number } = person;
@@ -92,6 +109,24 @@ app.post("/api/persons", (request, response) => {
   newPerson.id = Math.trunc(Math.random() * 10000);
   persons = persons.concat(newPerson);
   response.status(201).json(newPerson);
+});
+*/
+
+app.post("/api/persons", (request, response) => {
+  const { name, number } = request.body;
+
+  if (!name || !number) {
+    return response.status(400).json({ error: "Missing name or number" });
+  }
+
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then((savedPerson) => {
+    response.status(201).json(savedPerson);
+  });
 });
 
 app.put("/api/persons", (request, response) => {
