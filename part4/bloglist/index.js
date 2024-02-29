d51fbf39e6fa2202aa93973a46bdc381d6e70dac
JoseValperga <jose.valperga@gmail.com>
2024-02-29 13:49:26 -0300

    exercise 4.2 - continue

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,15 +2,15 @@ const { PORT, MONGODB_URI } = require("./utils/config")
 const express = require('express')
 const app = express()
 const cors = require('cors')
-//const blogRoutes = require("./controllers/blogs")
+const blogRoutes = require("./controllers/blogs")
 const Blog = require("./models/blog")
 
-//const midleware
 const logger = require("./utils/logger")
 const mongoose = require('mongoose')
 mongoose.set("strictQuery", false);
 
 logger.info("connecting to", MONGODB_URI);
+
 mongoose
     .connect(MONGODB_URI)
     .then((result) => {
@@ -23,24 +23,7 @@ mongoose
 app.use(cors())
 app.use(express.static("dist"));
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogRoutes)
 
 app.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`)
