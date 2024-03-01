df21c39903abb39b98ecacf6af2787ca0e0c63e5
JoseValperga <jose.valperga@gmail.com>
2024-03-01 11:55:55 -0300

    exercises 4.3 4.5 ready

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,9 +6,19 @@ const dummy = (blogs) => {
 const totalLikes = (blogList) => {
   let total = blogList.reduce((acumulador, objeto) => acumulador + objeto.likes, 0);
   return total;
+};
+
+const favoriteBlog = (blogList) => {
+  let objetoConMasLikes = blogList.reduce((objetoConMasLikes, objetoActual) => {
+    return objetoActual.likes > objetoConMasLikes.likes ? objetoActual : objetoConMasLikes;
+  }, blogList[0]);
+  return objetoConMasLikes;
 
 };
+
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
