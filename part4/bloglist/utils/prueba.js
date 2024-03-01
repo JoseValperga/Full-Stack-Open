20eb820b018b53d8d68013775448aec008cc1321
JoseValperga <jose.valperga@gmail.com>
2024-03-01 15:30:42 -0300

    working...

--- part4/bloglist/utils/prueba.js
+++ part4/bloglist/utils/prueba.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 const blogList = [
   {
     _id: "5a422a851b54a676234d17f7",
@@ -6,7 +5,7 @@ const blogList = [
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422aa71b54a676234d17f8",
@@ -14,7 +13,7 @@ const blogList = [
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422b3a1b54a676234d17f9",
@@ -22,7 +21,7 @@ const blogList = [
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     likes: 12,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422b891b54a676234d17fa",
@@ -30,7 +29,7 @@ const blogList = [
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
     likes: 10,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422ba71b54a676234d17fb",
@@ -38,7 +37,7 @@ const blogList = [
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
     likes: 0,
-    __v: 0
+    __v: 0,
   },
   {
     _id: "5a422bc61b54a676234d17fc",
@@ -46,47 +45,24 @@ const blogList = [
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
-    __v: 0
-  }
+    __v: 0,
+  },
 ];
-
-const dummy = (blogs) => {
-  return 1;
-};
-
-const totalLikes = (blogList) => {
-  let total = blogList.reduce(
-    (acumulador, objeto) => acumulador + objeto.likes,
-    0
-  );
-  return total;
-};
-
-const favoriteBlog = (blogList) => {
-  let objetoConMasLikes = blogList.reduce((objetoConMasLikes, objetoActual) => {
-    return objetoActual.likes > objetoConMasLikes.likes
-      ? objetoActual
-      : objetoConMasLikes;
-  }, blogList[0]);
-  return objetoConMasLikes;
-};
 
 const mostBlogs = (blogList) => {
   let result = [{ author: blogList[0].author, blogs: 1 }];
+
   for (let i = 1; i < blogList.length; i++) {
-    for (let j = 0; j < result.length; j++)
+    for (let j = 0; j < result.length; j++) {
+        console.log(result)
       blogList[i].author === result[j].author
         ? result[j].blogs++
         : result.push({ author: blogList[i].author, blogs: 1 });
+    
+    }
   }
+  return result;
 };
 
+console.log(mostBlogs(blogList));
 
-module.exports = {
-  dummy,
-  totalLikes,
-  favoriteBlog,
-  mostBlogs,
-};
-
-mostBlogs(blogList);
